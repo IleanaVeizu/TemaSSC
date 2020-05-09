3179fca138df784a5a274ff56c1aca1a2c45cacf
IleanaVeizu <35433542+IleanaVeizu@users.noreply.github.com>
2020-05-09 18:23:48 +0300

    files for sssc homework

---
+++ homepage.php
@@ -0,0 +1,24 @@
+<html>
+<!-- Text between angle brackets is an HTML tag and is not displayed.
+Most tags, such as the HTML and /HTML tags that surround the contents of
+a page, come in pairs; some tags, like HR, for a horizontal rule, stand 
+alone. Comments, such as the text you're reading, are not displayed when
+the Web page is shown. The information between the HEAD and /HEAD tags is 
+not displayed. The information between the BODY and /BODY tags is displayed.-->
+<head>
+<title>BESTshow</title>
+</head>
+<!-- The information between the BODY and /BODY tags is displayed.-->
+<body>
+<h1>This will be the next page of my thesis.</h1>
+<p>Be <b>bold</b> but unfortunatelly is not ready yet, so I'll leave you with this: </p>
+<ul>
+<li>A super nice book</li>
+<li>And <i>a super nice</i> song <a href="https://www.youtube.com/watch?v=wccRif2DaGs"> over here </a></li>
+</ul>
+<p><img src="book.png" alt="A Great HTML Resource"></p>
+<hr>
+<p>Finally, thank you!</p>
+<!-- And add a copyright notice.-->
+</body>
+</html>
